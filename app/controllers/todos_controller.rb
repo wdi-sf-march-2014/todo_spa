db52a0c512c71f792c5b23c124f84dda8f3d32a5
RahulKhanna <sociallyrk@gmail.com>
2014-06-02 21:49:36 -0700

    todos delete works

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,4 +17,20 @@ class TodosController < ApplicationController
   	end
   end
 
+
+  def update
+    todo_params = params.require(:todo).permit(:title, :completed, :id)
+    @todo = Todo.find(params[:id])
+    @todo.update todo_params
+    respond_to do |f|
+      f.json { render :json => @todo, only: [:id, :title, :completed]}
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id]).destroy
+    respond_to do |f|
+      f.json { render :json => @todo, only: [:id, :title, :completed]}
+    end
+  end
 end
